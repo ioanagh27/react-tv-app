7b0e0c3da6350f2edc526daba69bbc0e3373589e
Ioana Gheorghias <ioana.gh27@gmail.com>
2022-08-23 10:59:16 +0100

    Add api

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import './App.css';
+
+import ShowCard from './component/ShowCard.jsx'
+
 
 const App = () => {
+    const [showData, setShowData] = useState([]);
+
+    useEffect(() => {
+
+        async function searchApi() {
+            const result = await axios.get("https://api.tvmaze.com/search/shows?q=girls")    ;
+            setShowData(result.data)        
+        }
+
+        searchApi();
+
+    }, [])
     
-    return <h1>Hello, World!</h1>
+    return  <>
+            <form id="search-form"></form>
+            {showData.map((s)=> <ShowCard key={s["show"].id} data={s["show"]} />)}
+            </>
 
 
 
